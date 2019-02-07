20a6b1480b677e50b960442a52e0d4e3d5d6caf0
Lavanya Chandramouli <lchandramouli@google.com>
2019-02-07 15:48:05 -0800

    Cleanup: Use assert and delete methods from common IT class.
    Change-Id: If5ddbb0bc2be878b6eb68c241ad75fc8c5e179e7

--- src/it/java/com/google/enterprise/cloudsearch/database/DatabaseConnectorIT.java
+++ src/it/java/com/google/enterprise/cloudsearch/database/DatabaseConnectorIT.java
@@ -16,11 +16,14 @@
 
 package com.google.enterprise.cloudsearch.database;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.enterprise.cloudsearch.sdk.TestProperties.SERVICE_KEY_PROPERTY_NAME;
 import static com.google.enterprise.cloudsearch.sdk.TestProperties.qualifyTestProperty;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import com.google.api.services.cloudsearch.v1.model.Item;
+import com.google.common.base.Strings;
 import com.google.enterprise.cloudsearch.sdk.Util;
 import com.google.enterprise.cloudsearch.sdk.config.Configuration.ResetConfigRule;
 import com.google.enterprise.cloudsearch.sdk.indexing.CloudSearchService;
@@ -29,6 +32,7 @@ import com.google.enterprise.cloudsearch.sdk.indexing.IndexingApplication;
 import com.google.enterprise.cloudsearch.sdk.indexing.IndexingItemBuilder.ItemType;
 import com.google.enterprise.cloudsearch.sdk.indexing.MockItem;
 import com.google.enterprise.cloudsearch.sdk.indexing.StructuredDataHelper;
+import com.google.enterprise.cloudsearch.sdk.indexing.TestUtils;
 import com.google.enterprise.cloudsearch.sdk.indexing.template.FullTraversalConnector;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -67,6 +71,7 @@ public class DatabaseConnectorIT {
   private static String indexingSourceId;
   private static Optional<String> rootUrl;
   private static CloudSearchService v1Client;
+  private static TestUtils testUtils;
 
   @Rule public ResetConfigRule resetConfig = new ResetConfigRule();
   @Rule public TemporaryFolder configFolder = new TemporaryFolder();
@@ -77,6 +82,7 @@ public class DatabaseConnectorIT {
     validateInputParams();
     v1Client = new CloudSearchService(keyFilePath, indexingSourceId, rootUrl);
     StructuredDataHelper.verifyMockContentDatasourceSchema(v1Client.getSchema());
+    testUtils = new TestUtils(v1Client);
   }
 
   private String getDBUrl() throws IOException {
@@ -125,9 +131,8 @@ public class DatabaseConnectorIT {
     try {
       dataSourceId = System.getProperty(DATA_SOURCE_ID_PROPERTY_NAME);
       serviceKeyPath = Paths.get(System.getProperty(SERVICE_KEY_PROPERTY_NAME));
-      assertThat(serviceKeyPath.toFile().exists()).isTrue();
-      assertThat(dataSourceId).isNotNull();
-      assertThat(dataSourceId).isNotEmpty();
+      assertTrue(serviceKeyPath.toFile().exists());
+      assertFalse(Strings.isNullOrEmpty(dataSourceId));
       rootUrl = Optional.ofNullable(System.getProperty(ROOT_URL_PROPERTY_NAME));
     } catch (AssertionError error) {
       logger.log(Level.SEVERE,
@@ -166,6 +171,7 @@ public class DatabaseConnectorIT {
     String row2 = "x2" + randomId;
     String row3 = "x3" + randomId;
     List<String> query = new ArrayList<>();
+    List<String> mockItems = new ArrayList<>();
     query.add("create table " + tableName
         + "(id varchar(32) unique not null, name varchar(128), phone varchar(16))");
     query.add("insert into " + tableName + " (id, name, phone)"
@@ -174,29 +180,34 @@ public class DatabaseConnectorIT {
         + " values ('" + row2 + "', 'Joe Smith', '9848')");
     query.add("insert into " + tableName + " (id, name, phone)"
         + " values ('" + row3 + "', 'Mike Brown', '3476')");
-    String[] args = setupDataAndConfiguration(config, query);
-    MockItem itemId1 = new MockItem.Builder(getItemId(row1))
-        .setTitle(row1)
-        .setSourceRepositoryUrl(row1)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .build();
-    MockItem itemId2 = new MockItem.Builder(getItemId(row2))
-        .setTitle(row2)
-        .setSourceRepositoryUrl(row2)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .build();
-    MockItem itemId3 = new MockItem.Builder(getItemId(row3))
-        .setTitle(row3)
-        .setSourceRepositoryUrl(row3)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .build();
-    runAwaitConnector(args);
-    getAndAssertItem(v1Client.getItem(getItemId(row1)), itemId1.getItem());
-    getAndAssertItem(v1Client.getItem(getItemId(row2)), itemId2.getItem());
-    getAndAssertItem(v1Client.getItem(getItemId(row3)), itemId3.getItem());
+    try {
+      String[] args = setupDataAndConfiguration(config, query);
+      MockItem itemId1 = new MockItem.Builder(getItemId(row1))
+          .setTitle(row1)
+          .setSourceRepositoryUrl(row1)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .build();
+      MockItem itemId2 = new MockItem.Builder(getItemId(row2))
+          .setTitle(row2)
+          .setSourceRepositoryUrl(row2)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .build();
+      MockItem itemId3 = new MockItem.Builder(getItemId(row3))
+          .setTitle(row3)
+          .setSourceRepositoryUrl(row3)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .build();
+      mockItems.addAll(asList(getItemId(row1), getItemId(row2), getItemId(row3)));
+      runAwaitConnector(args);
+      testUtils.waitUntilEqual(getItemId(row1), itemId1.getItem());
+      testUtils.waitUntilEqual(getItemId(row2), itemId2.getItem());
+      testUtils.waitUntilEqual(getItemId(row3), itemId3.getItem());
+    } finally {
+      v1Client.deleteItemsIfExist(mockItems);
+    }
   }
 
   @Test
@@ -217,6 +228,7 @@ public class DatabaseConnectorIT {
     String row3 = "s3" + randomId;
 
     List<String> query = new ArrayList<>();
+    List<String> mockItems = new ArrayList<>();
     query.add("create table " + tableName + "(id varchar(32) unique not null,"
         + " textField varchar(128), integerField integer(50), booleanField boolean,"
         + " doubleField double, dateField date, timestampField timestamp, enumField integer)");
@@ -232,54 +244,59 @@ public class DatabaseConnectorIT {
         + " (id, textField, integerField, booleanField, doubleField, dateField, timestampField,"
         + " enumField) values ('" + row3 + "', 'Mike Smith', '9358014', 'true', '-9000.00',"
             + " '1940-11-11', '1817-10-10T14:21:23.040Z', '2')");
-    String[] args = setupDataAndConfiguration(config, query);
+    try {
+      String[] args = setupDataAndConfiguration(config, query);
 
-    MockItem itemId1 = new MockItem.Builder(getItemId(row1))
-        .setTitle(row1)
-        .setSourceRepositoryUrl("http://example.com/employee/" + row1)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .addValue("text", "Jones May")
-        .addValue("integer", "2134678")
-        .addValue("boolean", "true")
-        .addValue("date", "2007-11-20")
-        .addValue("double", "2000.00")
-        .setObjectType("myMockDataObject")
-        .addValue("timestamp", "1907-10-10T14:21:23.400Z")
-        .addValue("enum", 2)
-        .build();
-    MockItem itemId2 = new MockItem.Builder(getItemId(row2))
-        .setTitle(row2)
-        .setSourceRepositoryUrl("http://example.com/employee/" + row2)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .addValue("text", "Joe Smith")
-        .addValue("integer", "-9846")
-        .addValue("boolean", "false")
-        .addValue("date", "1987-02-28")
-        .addValue("double", "12000.00")
-        .setObjectType("myMockDataObject")
-        .addValue("timestamp", "2017-10-10T14:01:23.400Z")
-        .addValue("enum", 1)
-        .build();
-    MockItem itemId3 = new MockItem.Builder(getItemId(row3))
-        .setTitle(row3)
-        .setSourceRepositoryUrl("http://example.com/employee/" + row3)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .addValue("text", "Mike Smith")
-        .addValue("integer", "9358014")
-        .addValue("boolean", "true")
-        .addValue("date", "1940-11-11")
-        .addValue("double", "-9000.00")
-        .setObjectType("myMockDataObject")
-        .addValue("timestamp", "1817-10-10T14:21:23.040Z")
-        .addValue("enum", 2)
-        .build();
-    runAwaitConnector(args);
-    verifyStructuredData(getItemId(row1), "myMockDataObject", itemId1.getItem());
-    verifyStructuredData(getItemId(row2), "myMockDataObject", itemId2.getItem());
-    verifyStructuredData(getItemId(row3), "myMockDataObject", itemId3.getItem());
+      MockItem itemId1 = new MockItem.Builder(getItemId(row1))
+          .setTitle(row1)
+          .setSourceRepositoryUrl("http://example.com/employee/" + row1)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .addValue("text", "Jones May")
+          .addValue("integer", "2134678")
+          .addValue("boolean", "true")
+          .addValue("date", "2007-11-20")
+          .addValue("double", "2000.00")
+          .setObjectType("myMockDataObject")
+          .addValue("timestamp", "1907-10-10T14:21:23.400Z")
+          .addValue("enum", 2)
+          .build();
+      MockItem itemId2 = new MockItem.Builder(getItemId(row2))
+          .setTitle(row2)
+          .setSourceRepositoryUrl("http://example.com/employee/" + row2)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .addValue("text", "Joe Smith")
+          .addValue("integer", "-9846")
+          .addValue("boolean", "false")
+          .addValue("date", "1987-02-28")
+          .addValue("double", "12000.00")
+          .setObjectType("myMockDataObject")
+          .addValue("timestamp", "2017-10-10T14:01:23.400Z")
+          .addValue("enum", 1)
+          .build();
+      MockItem itemId3 = new MockItem.Builder(getItemId(row3))
+          .setTitle(row3)
+          .setSourceRepositoryUrl("http://example.com/employee/" + row3)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .addValue("text", "Mike Smith")
+          .addValue("integer", "9358014")
+          .addValue("boolean", "true")
+          .addValue("date", "1940-11-11")
+          .addValue("double", "-9000.00")
+          .setObjectType("myMockDataObject")
+          .addValue("timestamp", "1817-10-10T14:21:23.040Z")
+          .addValue("enum", 2)
+          .build();
+      mockItems.addAll(asList(getItemId(row1), getItemId(row2), getItemId(row3)));
+      runAwaitConnector(args);
+      verifyStructuredData(getItemId(row1), "myMockDataObject", itemId1.getItem());
+      verifyStructuredData(getItemId(row2), "myMockDataObject", itemId2.getItem());
+      verifyStructuredData(getItemId(row3), "myMockDataObject", itemId3.getItem());
+    } finally {
+      v1Client.deleteItemsIfExist(mockItems);
+    }
   }
 
   @Test
@@ -306,6 +323,7 @@ public class DatabaseConnectorIT {
     String row1 = "row1" + randomId;
     String row2 = "row2" + randomId;
     List<String> query = new ArrayList<>();
+    List<String> mockItems = new ArrayList<>();
     query.add("create table " + tableName + "(id varchar(32) unique not null, title varchar(100), "
         + " name varchar(128), phone integer, created timestamp, modified timestamp,"
         + " language varchar(10))");
@@ -314,25 +332,30 @@ public class DatabaseConnectorIT {
             + " '2018-10-10T14:21:23.400Z', 'de-ch')");
     query.add("insert into " + tableName + " (id, name, phone)"
         + " values ('" + row2 + "', 'Mike Smith', '9848')");
-    String[] args = setupDataAndConfiguration(config, query);
-    MockItem itemId1 = new MockItem.Builder(getItemId(row1))
-        .setTitle("TitleRow1")
-        .setSourceRepositoryUrl("https://DEFAULT.URL/" + row1)
-        .setContentLanguage("de-ch")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .setCreateTime("1907-10-10T14:21:23.400Z")
-        .setUpdateTime("2018-10-10T14:21:23.400Z")
-        .build();
-    MockItem itemId2 = new MockItem.Builder(getItemId(row2))
-        .setTitle("DEFAULT_TITLE")
-        .setSourceRepositoryUrl("https://DEFAULT.URL/" + row2)
-        .setContentLanguage("fr-CA")
-        .setCreateTime("2007-10-10T14:21:23.400Z")
-        .setUpdateTime("1919-10-10T14:21:23.400Z")
-        .build();
-    runAwaitConnector(args);
-    getAndAssertItem(v1Client.getItem(getItemId(row1)), itemId1.getItem());
-    getAndAssertItem(v1Client.getItem(getItemId(row2)), itemId2.getItem());
+    try {
+      String[] args = setupDataAndConfiguration(config, query);
+      MockItem itemId1 = new MockItem.Builder(getItemId(row1))
+          .setTitle("TitleRow1")
+          .setSourceRepositoryUrl("https://DEFAULT.URL/" + row1)
+          .setContentLanguage("de-ch")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .setCreateTime("1907-10-10T14:21:23.400Z")
+          .setUpdateTime("2018-10-10T14:21:23.400Z")
+          .build();
+      MockItem itemId2 = new MockItem.Builder(getItemId(row2))
+          .setTitle("DEFAULT_TITLE")
+          .setSourceRepositoryUrl("https://DEFAULT.URL/" + row2)
+          .setContentLanguage("fr-CA")
+          .setCreateTime("2007-10-10T14:21:23.400Z")
+          .setUpdateTime("1919-10-10T14:21:23.400Z")
+          .build();
+      mockItems.addAll(asList(getItemId(row1), getItemId(row2)));
+      runAwaitConnector(args);
+      testUtils.waitUntilEqual(getItemId(row1), itemId1.getItem());
+      testUtils.waitUntilEqual(getItemId(row2), itemId2.getItem());
+    } finally {
+      v1Client.deleteItemsIfExist(mockItems);
+    }
   }
 
   @Test
@@ -350,6 +373,7 @@ public class DatabaseConnectorIT {
     String row2 = "row2" + randomId;
 
     List<String> query = new ArrayList<>();
+    List<String> mockItems = new ArrayList<>();
     query.add("create table " + tableName + "(id varchar(32) unique not null,"
         + " arrayTextField array, arrayIntegerField array)");
     query.add("insert into " + tableName
@@ -358,42 +382,45 @@ public class DatabaseConnectorIT {
     query.add("insert into " + tableName
         + " (id, arrayTextField, arrayIntegerField) values"
         + " ('" + row2 + "', ('Jim', 'Black'), ('1092873', '-128765'))");
-    String[] args = setupDataAndConfiguration(config, query);
-
-    MockItem itemId1 = new MockItem.Builder(getItemId(row1))
-        .setTitle(row1)
-        .setSourceRepositoryUrl("http://example.com/employee/" + row1)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .addValue("text", "joe")
-        .addValue("text", "Smith")
-        .addValue("text", "black")
-        .addValue("integer", "1092")
-        .addValue("integer", "8765")
-        .setObjectType("myMockDataObject")
-        .build();
-   MockItem itemId2 = new MockItem.Builder(getItemId(row2))
-        .setTitle(row2)
-        .setSourceRepositoryUrl("http://example.com/employee/" + row2)
-        .setContentLanguage("en-US")
-        .setItemType(ItemType.CONTENT_ITEM.toString())
-        .addValue("text", "Jim")
-        .addValue("text", "Black")
-        .addValue("integer", "1092873")
-        .addValue("integer", "-128765")
-        .setObjectType("myMockDataObject")
-        .build();
-    runAwaitConnector(args);
-    verifyStructuredData(getItemId(row1), "myMockDataObject", itemId1.getItem());
-    verifyStructuredData(getItemId(row2), "myMockDataObject", itemId2.getItem());
+    try {
+      String[] args = setupDataAndConfiguration(config, query);
+      MockItem itemId1 = new MockItem.Builder(getItemId(row1))
+          .setTitle(row1)
+          .setSourceRepositoryUrl("http://example.com/employee/" + row1)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .addValue("text", "joe")
+          .addValue("text", "Smith")
+          .addValue("text", "black")
+          .addValue("integer", "1092")
+          .addValue("integer", "8765")
+          .setObjectType("myMockDataObject")
+          .build();
+     MockItem itemId2 = new MockItem.Builder(getItemId(row2))
+          .setTitle(row2)
+          .setSourceRepositoryUrl("http://example.com/employee/" + row2)
+          .setContentLanguage("en-US")
+          .setItemType(ItemType.CONTENT_ITEM.toString())
+          .addValue("text", "Jim")
+          .addValue("text", "Black")
+          .addValue("integer", "1092873")
+          .addValue("integer", "-128765")
+          .setObjectType("myMockDataObject")
+          .build();
+      mockItems.addAll(asList(getItemId(row1), getItemId(row2)));
+      runAwaitConnector(args);
+      verifyStructuredData(getItemId(row1), "myMockDataObject", itemId1.getItem());
+      verifyStructuredData(getItemId(row2), "myMockDataObject", itemId2.getItem());
+    } finally {
+      v1Client.deleteItemsIfExist(mockItems);
+    }
   }
 
-  private void verifyStructuredData(String itemId, String schemaObjectType,
-      Item expectedItem) throws IOException {
-    Item actualItem = v1Client.getItem(itemId);
-    logger.log(Level.INFO, "Verifying actualItem: {0}", actualItem);
-    StructuredDataHelper.assertStructuredData(actualItem, expectedItem, schemaObjectType);
-    getAndAssertItem(actualItem, expectedItem);
+  private void verifyStructuredData(String itemId, String schemaObjectType, Item expectedItem)
+      throws IOException {
+    testUtils.waitUntilEqual(itemId, expectedItem);
+    StructuredDataHelper.assertStructuredData(v1Client.getItem(itemId),
+        expectedItem, schemaObjectType);
   }
 
   private void runAwaitConnector(String[] args) throws InterruptedException {
@@ -407,19 +434,6 @@ public class DatabaseConnectorIT {
         .build();
     dbConnector.start();
     return dbConnector;
-  }
-
-  private void getAndAssertItem(Item actualItem, Item expectedItem) throws IOException {
-    logger.log(Level.INFO, "Verifying actualItem: {0}", actualItem);
-    logger.log(Level.INFO, "Verifying expectedItem: {0}", expectedItem);
-    try {
-      assertThat(actualItem.getStatus().getCode()).isEqualTo("ACCEPTED");
-      assertThat(actualItem.getItemType()).isEqualTo(expectedItem.getItemType());
-      assertThat(actualItem.getMetadata()).isEqualTo(expectedItem.getMetadata());
-      assertThat(actualItem.getName()).isEqualTo(expectedItem.getName());
-    } finally {
-      v1Client.deleteItem(actualItem.getName(), actualItem.getVersion());
-    }
   }
 
   private String getItemId(String name) {
